5a8fa4ec3e55a277810072adb900d02ec0cdcd16
weriomat <engel@weriomat.com>
2025-03-23 11:53:21 +0100

    chore: refactor upstream fetching of narlist, creating of uri

--- harmonia/src/narlist.rs
+++ harmonia/src/narlist.rs
@@ -14,90 +14,81 @@ pub(crate) async fn get(
 ) -> Result<HttpResponse, Box<dyn Error>> {
     eprintln!("Got request for {}", hash);
 
-    let narlist = match nixhash(&hash) {
-        Some(store_path) => {
-            println!("Found in store");
-            libnixstore::get_nar_list(&store_path)?
-        }
-        None => {
-            // Fetch from upstream
-            let header_narlist = HeaderValue::from_str("application/json")
-                .expect("narinfo conversion failed for header");
+    if let Some(store_path) = nixhash(&hash) {
+        let narlist = libnixstore::get_nar_list(&store_path)?;
 
-            for upstream in &settings.upstreams {
-                println!("Upstream {upstream:?}");
-                let mut uri = upstream.url.clone().into_parts();
-                uri.path_and_query = Some(uri.path_and_query.map_or(
-                    format!("/{hash}.ls").parse::<PathAndQuery>().unwrap(),
-                    |url| {
-                        format!("{}{}.ls", url.path(), hash)
-                            .parse::<PathAndQuery>()
-                            .unwrap()
-                    },
-                ));
+        return Ok(HttpResponse::Ok()
+            .insert_header(cache_control_max_age_1y())
+            .insert_header(http::header::ContentType(mime::APPLICATION_JSON))
+            .body(narlist));
+    }
 
-                match Client::new()
-                    .request(http::Method::GET, Uri::try_from(uri).unwrap())
-                    .timeout(Duration::from_secs(5))
-                    .no_decompress()
-                    .send()
-                    .await
-                {
-                    Ok(response) => match response.status() == http::StatusCode::OK {
-                        true => {
-                            let headers = response.headers();
+    // Fetch from upstream
+    let header_narlist =
+        HeaderValue::from_str("application/json").expect("narinfo conversion failed for header");
 
-                            eprintln!("Headers: {headers:?}");
+    for upstream in &settings.upstreams {
+        println!("Upstream {upstream:?}");
 
-                            let ct = match headers.get(http::header::CONTENT_TYPE) {
-                                Some(ct) => match ct == header_narlist {
-                                    true => ct,
-                                    false => {
-                                        eprintln!("Received invalid content-type from upstream");
-                                        continue;
-                                    }
-                                },
-                                None => {
-                                    eprintln!("Received invalid content-type from upstream");
-                                    continue;
-                                }
-                            };
+        let mut uri_parts = upstream.url.clone().into_parts();
+        uri_parts.path_and_query = Some(format!("/{hash}.ls").parse::<PathAndQuery>()?);
+        let uri = Uri::try_from(uri_parts)?;
 
-                            // TODO: handle content length? Date?
-                            match headers.get(http::header::CONTENT_ENCODING) {
-                                Some(ce) => {
-                                    return Ok(HttpResponse::Ok()
-                                        .insert_header(cache_control_max_age_1d())
-                                        .insert_header((http::header::CONTENT_TYPE, ct))
-                                        .insert_header((http::header::CONTENT_ENCODING, ce))
-                                        .streaming(response));
-                                }
-                                None => {
-                                    return Ok(HttpResponse::Ok()
-                                        .insert_header(cache_control_max_age_1d())
-                                        .insert_header((http::header::CONTENT_TYPE, ct))
-                                        .streaming(response));
-                                }
+        match Client::new()
+            .request(http::Method::GET, uri)
+            .timeout(Duration::from_secs(5))
+            .no_decompress()
+            .send()
+            .await
+        {
+            Ok(response) => match response.status() == http::StatusCode::OK {
+                true => {
+                    let headers = response.headers();
+
+                    let ct = match headers.get(http::header::CONTENT_TYPE) {
+                        Some(ct) => match ct == header_narlist {
+                            true => ct,
+                            false => {
+                                eprintln!("Received invalid content-type from upstream");
+                                continue;
                             }
+                        },
+                        None => {
+                            eprintln!("Received invalid content-type from upstream");
+                            continue;
                         }
-                        false => {
-                            eprintln!("Failed to make cache reqest: {response:?}");
+                    };
+
+                    // TODO: handle content length? Date?
+                    match headers.get(http::header::CONTENT_ENCODING) {
+                        Some(ce) => {
+                            return Ok(HttpResponse::Ok()
+                                .insert_header(cache_control_max_age_1d())
+                                .insert_header((http::header::CONTENT_TYPE, ct))
+                                .insert_header((http::header::CONTENT_ENCODING, ce))
+                                .streaming(response));
                         }
-                    },
-                    Err(err) => {
-                        eprintln!("Failed to construct cache reqest: {err:?}")
+                        None => {
+                            return Ok(HttpResponse::Ok()
+                                .insert_header(cache_control_max_age_1d())
+                                .insert_header((http::header::CONTENT_TYPE, ct))
+                                .streaming(response));
+                        }
                     }
                 }
+                false => {
+                    eprintln!("Failed to make cache reqest: {response:?}");
+                    continue;
+                }
+            },
+            Err(err) => {
+                eprintln!("Failed to construct cache reqest: {err:?}");
+                continue;
             }
-
-            return Ok(HttpResponse::NotFound()
-                .insert_header(crate::cache_control_no_store())
-                .body("missed hash for narlist"));
         }
-    };
+    }
 
-    Ok(HttpResponse::Ok()
-        .insert_header(cache_control_max_age_1y())
-        .insert_header(http::header::ContentType(mime::APPLICATION_JSON))
-        .body(narlist))
+    Ok(HttpResponse::NotFound()
+        .insert_header(crate::cache_control_no_store())
+        .body("missed hash for narlist"))
 }
